00f6e0a2f90660879585b83c3a05f905d6e05a1e
just2so <169085685+just2so@users.noreply.github.com>
2024-10-19 17:33:09 +0800

    Create update_database.sh

---
+++ update_database.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 生成新的 UUID
+NEW_UUID=$(uuidgen)
+
+# 执行 SQLite 更新命令
+sqlite3 /usr/local/h-ui/data/h_ui.db <<EOF
+UPDATE account
+SET con_pass = 'zhangsan.$NEW_UUID'
+WHERE username = 'zhangsan';
+EOF
