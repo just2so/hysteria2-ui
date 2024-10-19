dd2562717138cd93160d84333a7dc5979c2da0e1
just2so <169085685+just2so@users.noreply.github.com>
2024-10-19 18:56:19 +0800

    Update update_database.sh

--- update_database.sh
+++ update_database.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# 生成新的 UUID
 NEW_UUID=$(uuidgen)
 
 echo "Generated UUID: $NEW_UUID"
 
-# 获取用户名（通过环境变量）
+# 获取用户名，通过环境变量
 USERNAME=${USERNAME}
 
-# 执行 SQLite 更新命令并捕获影响的行数
 ROWS_AFFECTED=$(sqlite3 /usr/local/h-ui/data/h_ui.db <<EOF
 UPDATE account
 SET con_pass = '$USERNAME.$NEW_UUID'
@@ -17,7 +15,6 @@ SELECT changes();
 EOF
 )
 
-# 检查影响的行数是否大于 0
 if [ "$ROWS_AFFECTED" -gt 0 ]; then
   echo "Database updated successfully. Rows affected: $ROWS_AFFECTED"
 else
